540ae15c111c641b11f465b8be54557316e8eb59
Teyko <theovilainfr@gmail.com>
2022-01-17 16:36:13 +0100

    0.1

---
+++ index.php
@@ -0,0 +1,68 @@
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="styles/index.css" type="text/css">
+        <link rel="stylesheet" href="styles/wamiclass.css" type="text/css">
+        <link rel="apple-touch-icon" sizes="180x180" href="././img/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="././img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="194x194" href="././img/favicon/favicon-194x194.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="././img/favicon/android-chrome-192x192.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="././img/favicon/favicon-16x16.png">
+        <link rel="manifest" href="././img/favicon/site.webmanifest">
+        <link rel="mask-icon" href="././img/favicon/safari-pinned-tab.svg" color="#000000">
+        <link rel="shortcut icon" href="././img/favicon/favicon.ico">
+        <meta name="msapplication-TileColor" content="#000000">
+        <meta name="msapplication-TileImage" content="././img/favicon/mstile-144x144.png">
+        <meta name="msapplication-config" content="././img/favicon/browserconfig.xml">
+        <meta name="theme-color" content="#000000">
+        <title>WAMIGeo - Accueil</title>
+    </head>
+    <body>
+        <div id="starsBG">
+            <div id="logo">
+                <img src="img/wami-logo.png" id="wamilogo" alt="">
+            </div>
+            <div id="earthImg"></div>
+            <div id="navbarTop">
+                <a href="#" class="navbarTop">Jouer</a>
+                <a href="#" class="navbarTop">Communauté</a>
+                <a href="#" class="navbarTop">F.A.Q</a>
+                <a href="#" class="navbarTop" id="profil">Connexion / Inscription</a>
+            </div>
+            <h1 id="slogan">EXPLOREZ LE MONDE !</h1>
+            <p id="subSlogan">
+                Cherchez des indices pour trouver le bâtiment donné<br>
+                en début de chaque round. Affrontez vos amis ou<br>
+                grimpez dans le classement solo.
+            </p>
+            <button onclick="" id="signUp" class="button_blue_white">Rejoignez-vous !</button>
+        </div>
+        <div id="earthBG">
+            <h3 id="topTitle">ARRIVEZ AU TOP !</h3>
+            <p id="topParagraph">
+                Gagnez des parties et arrivez au sommet du classement<br>
+                de votre pays ou même du monde. Vous obtiendrez des<br>
+                récompenses équivalentes à votre grade à chaque fin<br>
+                de saison. Une saison dure un mois.
+            </p>
+            <h3 id="friendTitle">JOUEZ AVEC VOS AMIS !</h3>
+            <p id="friendParagraph">
+                Affrontez vos amis dans des parties customisées,<br>
+                choisissez la ville ou le pays dans lequel vous voulez<br>
+                vous battre et que le meilleur gagne.
+            </p>
+        </div>
+        <div id="joinBG">
+            <h3 id="testerTitle">Devenez testeur !</h3>
+            <p id="joinParagraph">
+                L'équipe de testeurs de WamiGeo recherche grandement à<br>
+                s'agrandir et vous êtes les bienvenus.<br>
+                Votre seule mission est de jouer régulièrement au jeu et<br>
+                de report tous les bugs et toutes les améliorations à faire.
+            </p>
+            <button onclick="" id="joinButton" class="button_blue">Devenir testeur</button>
+        </div>
+    </body>
+</html>
